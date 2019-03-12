1fbf55fe24ed7401106c4ecb7dd9d7ef765aa552
Николай Мишонин <mishonin.n@gmail.com>
2019-03-12 17:35:20 +0600

    базовая инфраструктура приложения

---
+++ BomberMan-SuperAI/BattleSolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DataProvider;
+
+namespace BomberMan_SuperAI
+{
+    public class BattleSolver : ISolver
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Answer(string instanseName, DateTime startTime, uint time, string board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataProvider/ISolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataProvider
+{
+    public interface ISolver
+    {
+        void Initialize();
+
+        string Answer(string instanseName, DateTime startTime, uint time, string board);
+        //event EventHandler<Board> BoardLoaded;
+    }
+}
